04fc851b326be14ff5b57c148cf6f6602c4f32c2
Jason Studstill <standstill64@gmail.com>
2015-07-07 14:16:43 -0700

    Implement exports.multiplication

--- calculator.js
+++ calculator.js
@@ -1,3 +1,14 @@
 exports.addition = function(x, y) {
   return x + y;
 };
+
+exports.multiplication = function(x, y) {
+  var argArray = [];
+  for (var key in arguments)
+    argArray.push(arguments[key]);
+  return argArray.reduce(function(total, current) {
+    return total * current;
+  });
+};
+
+console.log(exports.multiplication(2, 2, 5));
