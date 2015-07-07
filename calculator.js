76b3ecf9203b08c9ffda06b8d5ec4cbfe6d2eee3
Jason Studstill <standstill64@gmail.com>
2015-07-07 14:27:56 -0700

    Implement division function

--- calculator.js
+++ calculator.js
@@ -12,3 +12,13 @@ exports.multiplication = function(x, y) {
 };
 
 console.log(exports.multiplication(2, 2, 5));
+
+exports.division = function(x, y) {
+  if (process.argv[2] === 'man' || process.argv[2] === 'help' || arguments.length > 4) {
+    console.log('Usage: node calculator.js num1 num2 --> prints num1 / num2');
+    return;
+  }
+  return x / y;
+};
+
+console.log(exports.division(process.argv[2], process.argv[3]));
